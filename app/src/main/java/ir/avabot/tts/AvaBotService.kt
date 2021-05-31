b79789143249056394169a887344dca82cd07df8
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-06-01 01:07:32 +0430

    2021.06.01 - 1

--- app/src/main/java/ir/avabot/tts/AvaBotService.kt
+++ app/src/main/java/ir/avabot/tts/AvaBotService.kt
@@ -1,29 +1,37 @@
 package ir.avabot.tts
 
+import android.media.MediaPlayer
 import android.speech.tts.SynthesisCallback
 import android.speech.tts.SynthesisRequest
+import android.speech.tts.TextToSpeech
 import android.speech.tts.TextToSpeechService
+import android.widget.Toast
 
 // adb connect 192.168.1.4:
 
 class AvaBotService : TextToSpeechService() {
-    override fun onIsLanguageAvailable(lang: String?, country: String?, variant: String?): Int {
-        TODO("Not yet implemented")
+    companion object {
+        const val PERSIAN = "fas"
     }
 
-    override fun onGetLanguage(): Array<String> {
-        TODO("Not yet implemented")
-    }
+    override fun onIsLanguageAvailable(lang: String?, country: String?, variant: String?): Int =
+        when (lang) {
+            PERSIAN -> TextToSpeech.LANG_AVAILABLE
+            else -> TextToSpeech.LANG_NOT_SUPPORTED
+        }
 
-    override fun onLoadLanguage(lang: String?, country: String?, variant: String?): Int {
-        TODO("Not yet implemented")
-    }
+    override fun onGetLanguage(): Array<String> = arrayOf("fas", "pes", "")
+
+    override fun onLoadLanguage(lang: String?, country: String?, variant: String?): Int =
+        when (lang) {
+            PERSIAN -> TextToSpeech.LANG_AVAILABLE
+            else -> TextToSpeech.LANG_NOT_SUPPORTED
+        }
 
     override fun onStop() {
-        TODO("Not yet implemented")
     }
 
     override fun onSynthesizeText(request: SynthesisRequest?, callback: SynthesisCallback?) {
-        TODO("Not yet implemented")
+        Toast.makeText(applicationContext, request?.charSequenceText?.toString(), Toast.LENGTH_LONG).show()
     }
 }
